694c3d4ffd4ac42fd5e6405f8d336a72e6512525
Marine <marine.magnin73@gmail.com>
2024-01-15 14:54:59 +0100

    CSS : modification css et ajout couleurs

--- src/utils/style/colors.js
+++ src/utils/style/colors.js
@@ -1,22 +1,15 @@
 const colors = {
   primary: '#00bc77',
   linkPrimary: '#42b983',
-  // primaryDarken: '#E60000',
   secondary: '#2c3e50',
-  //   tertiary: '#2c3e50',
+  tertiary: '#5256ec',
+  tertiaryLight: '#5256ecb8',
   colorItemTitle: '#222',
   colorTextFooter: '#ccc',
   colorAlert: '#880808',
-  // colorInfosName: '#282D30',
-  // colorInfosQuantity: '#74798C',
-  // colorProtein: '#4AB8FF', // Blue
-  // colorGlucid: '#FDCC0C', // Yellow
-  // colorLipid: '#FD5181', // Pink
-  // colorAxis: '#9B9EAC',
   colorBorder: '#000000', // Black
   backgroundDark: '#12002b',
   backgroundLight: '#FFFFFF',
-  // backgroundCursor: '#C4C4C480',
 };
 
 export default colors;
